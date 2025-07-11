dee2ffecb86c095ea3581227f2f98c945af10a7f
Valeria Rojas Ortiz <valeria.roortiz@gmail.com>
2025-07-11 00:29:33 +0000

    creacion vista 1

---
+++ autos/webapp/controller/Main.controller.ts
@@ -0,0 +1,12 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace vro.training.autos.controller
+ */
+export default class Main extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+}
