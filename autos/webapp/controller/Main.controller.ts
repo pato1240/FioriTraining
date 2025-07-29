b1eb08f63599578f0fa58fa96e57813310e4028d
Valeria Rojas Ortiz <valeria.roortiz@gmail.com>
2025-07-29 03:23:37 +0000

    Filters working

--- autos/webapp/controller/Main.controller.ts
+++ autos/webapp/controller/Main.controller.ts
@@ -1,5 +1,5 @@
 import Controller from "sap/ui/core/mvc/Controller";
-import { Input$LiveChangeEvent } from "sap/m/Input";
+import Input, { Input$LiveChangeEvent } from "sap/m/Input";
 import Filter from "sap/ui/model/Filter";
 import FilterOperator from "sap/ui/model/FilterOperator";
 import Component from "../Component";
@@ -7,7 +7,9 @@ import Router from "sap/f/routing/Router";
 import Event from "sap/ui/base/Event";
 import ColumnListItem from "sap/m/ColumnListItem";
 import Context from "sap/ui/model/odata/v2/Context";
-import Path from "sap/suite/ui/commons/statusindicator/Path";
+import Table from "sap/m/Table";
+import ListBinding from "sap/ui/model/ListBinding";
+import DatePicker from "sap/m/DatePicker";
 
 
 /**
@@ -20,15 +22,106 @@ export default class Main extends Controller {
 
     }
 
-    public onLiveChange(event: Input$LiveChangeEvent): void {
-        const value = event.getParameter("value");
-        console.log(value);
+    public onFilter (): void {
 
         const filters = [];
-        if(value){
-            filters.push(new Filter("Folio", FilterOperator.Contains, value));
+
+        const folio = (this.byId("folio") as Input).getValue();
+        const centroEnt = (this.byId("centroEnt") as Input).getValue();
+        let ticket = (this.byId("ticket") as Input).getValue();
+        const centroSal = (this.byId("centroSal") as Input).getValue();
+        const material = (this.byId("material") as Input).getValue();
+        const numeroDoc = (this.byId("numeroDoc") as Input).getValue();
+        const placa = (this.byId("placa") as Input).getValue();
+        let entradaBascula = (this.byId("entradaBascula") as DatePicker).getValue();
+        let salidaBascula = (this.byId("salidaBascula") as DatePicker).getValue();
+
+        switch (ticket) {
+            case "interno":
+                ticket = "1"
+                break;
+            case "externo":
+                    ticket = "2"
+                    break;
+            default: ""
+                break;
         }
 
+        if(entradaBascula){
+            entradaBascula = this.formatDateToFilter(entradaBascula);
+        }
+
+        if (salidaBascula){
+            salidaBascula = this.formatDateToFilter(salidaBascula);
+        }
+
+
+        if(folio){
+            filters.push(new Filter("Folio", FilterOperator.EQ, folio));
+        }
+        if(centroEnt){
+            filters.push(new Filter("CentroEnt", FilterOperator.EQ, centroEnt));
+        }
+        if(ticket){
+            filters.push(new Filter("Tipoticket", FilterOperator.EQ, ticket));
+        }
+        if(centroSal){
+            filters.push(new Filter("CentroSal", FilterOperator.EQ, centroSal));
+        }
+        if(material){
+            filters.push(new Filter("Material", FilterOperator.Contains, material));
+        }
+        if(numeroDoc){
+            filters.push(new Filter("NumeroDoc", FilterOperator.EQ, numeroDoc));
+        }
+        if(placa){
+            filters.push(new Filter("Placa", FilterOperator.EQ, placa));
+        }
+        if(entradaBascula){
+            filters.push(new Filter("FechaEntBas", FilterOperator.Contains, entradaBascula));
+        }
+        if(salidaBascula){
+            filters.push(new Filter("FechaSalBas", FilterOperator.Contains, salidaBascula));
+        }
+
+        const resultsTable = this.byId("tablaResultado") as Table;
+        const binding = resultsTable.getBinding("items") as ListBinding;
+        binding.filter(filters);
+    }
+
+    private formatDateToFilter(fecha: string): string {
+        const split = fecha.split("/");
+            let mes = split[0];
+            let dia = split[1];
+            let anio = "20" + split[2];
+
+            if (mes.length === 1){
+                mes = "0" + mes
+            }
+
+            if (dia.length === 1){
+                dia = "0" + dia
+            }
+
+            fecha = anio + mes + dia;
+
+            return fecha;
+    }
+
+    public onAdaptFilters(): void {
+        (this.byId("folio") as Input).setValue("");
+        (this.byId("centroEnt") as Input).setValue("");
+        (this.byId("ticket") as Input).setValue("");
+        (this.byId("centroSal") as Input).setValue("");
+        (this.byId("material") as Input).setValue("");
+        (this.byId("numeroDoc") as Input).setValue("");
+        (this.byId("placa") as Input).setValue("");
+        (this.byId("entradaBascula") as DatePicker).setValue("");
+        (this.byId("salidaBascula") as DatePicker).setValue("");
+
+        const resultsTable = this.byId("tablaResultado") as Table;
+        const binding = resultsTable.getBinding("items") as ListBinding;
+        binding.filter([]);
     }
 
     public onNavToDetails(event: Event): void {
@@ -41,6 +134,5 @@ export default class Main extends Controller {
             numFolio: numFolio
         });
     }
-
 
 }
